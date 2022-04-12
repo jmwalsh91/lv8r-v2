29b467c14acb4ef6c27e17e060e7de63eb03c57b
jmwalsh91 <jordanmwalsh@gmail.com>
2022-04-12 02:42:15 -0600

    login and register pages, foundation component, todos

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,11 @@
-import NavBar from "~/components/NavBar";
-
+import { Link } from "@remix-run/react";
+//This will be the landing page, with a check for auth to reroute to dashboard on !auth .
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-<NavBar/>
-    </div>
+<p className="text-4xl text-secondary"> This is index route</p>
+<div className="text-2xl text-base glass">This will be rendered when no slug is present, so this is main.</div>
+<Link to="login"><button className="btn btn-primary">Click me for login</button>
+</Link></div>
   );
 }
---
+++ app/routes/login.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import {Form, Link, useActionData} from "@remix-run/react"
+
+
+//TODO: ACTION FUNCTION.
+//TODO: Validation and Error Handling
+//TODO: ^^ Optimistic UI. 
+//TODO: Login should not have props, do not use FC typing so we can avoid introducing typed children. Determine best practice and refactor. 
+function Login () {
+  
+  return (
+    
+<div className="card w-96 bg-neutral text-neutral-content my-10">
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Sign in to your account.</h2>
+<Form method="post">
+        <div className="form-control ">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <label className="input-group min-w-full">
+            <span>Email</span>
+            <input
+              type="text"
+              name="email"
+              placeholder="email@domain.com"
+              className="input input-bordered"
+            />
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <label className="input-group">
+            <span>Password</span>
+            <input
+              type="password"
+              name="password"
+              placeholder="password"
+              className="input input-bordered"
+            />
+          </label>
+</div> 
+          <div className="card-actions justify-around py-5">
+      
+      <button type="submit" className="btn btn-primary ">LogIn</button>
+    </div>
+        </Form>
+        {/* TODO: link to register */}
+        <Link to="/register"><button
+className="btn btn-ghost"
+>
+  
+Register
+</button>
+</Link>
+
+      </div>
+  
+    </div>
+  )
+}
+
+export default Login
