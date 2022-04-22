0067a6d1756de4f0693b3cc8e270f3a5ad75d983
jmwalsh91 <jordanmwalsh@gmail.com>
2022-04-22 03:26:08 -0600

    createPitch first page skel

--- app/routes/dashboard/$user/createPitch/index.tsx
+++ app/routes/dashboard/$user/createPitch/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 function index({}: Props) {
   return (
-    <div className=''>
-        <section className='card card-glass justify-center'>
-            <p className='text text-3xl text-primary-content'>
-                Make your pitch!
-            </p>
-        </section>
+    <div className="flex flex-col justify-items-center justify-center align-center">
+      <div>
+        <ul className="card steps rounded-2xl bg-base-100 w-full mb-3 rounded-sm">
+          <li className="step step-primary ">Cover</li>
+          <li className="step">Problem</li>
+          <li className="step">Solution</li>
+          <li className="step">Demo/CTA</li>
+        </ul>
+      </div>
+      <section className="card w-[80vw] h-[60vh] bg-base-100 justify-center">
+        <p className="text text-3xl text-secondary text-center">
+          Make your pitch!
+        </p>
+      </section>
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
