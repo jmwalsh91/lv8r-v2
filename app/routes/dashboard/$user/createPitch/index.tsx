6b9b91cdeb93287f214a071c7521ed3401585b54
jmwalsh91 <jordanmwalsh@gmail.com>
2022-04-25 23:59:40 -0600

    TODO in index of createpitch

--- app/routes/dashboard/$user/createPitch/index.tsx
+++ app/routes/dashboard/$user/createPitch/index.tsx
@@ -32,34 +32,7 @@ function CreatePitchIndex({}: Props) {
     let user = useLoaderData()
   return (
     <div className="flex flex-col justify-items-center justify-center align-center">
-      <div>
-        <ul className="card steps rounded-2xl bg-base-100 w-full mb-3 rounded-sm">
-          <li className="step step-primary ">Cover</li>
-          <li className="step">Problem</li>
-          <li className="step">Solution</li>
-          <li className="step">Demo/CTA</li>
-        </ul>
-      </div>
-      <section className="card w-[80vw] h-[60vh] bg-base-100 justify-center gap-8">
-        <p className="text text-3xl text-secondary text-center">
-          Make your pitch!
-        </p>
-
-        <Form method="post" action="?index" /* encType="multipart/form-data" */ className="flex flex-col justify-center gap-8">
-            <input type="hidden" value={user.user.id} name="ownerId"></input>
-          <input
-            type="text"
-            name="productName"
-            placeholder="Product name"
-            className="input input-bordered input-primary w-full max-w-xs"
-          />
-
-   {/* TODO: enctype on form, clone request in action so that 2's loader has access after the image upload function returns       <input type="file" name="image"></input> */}
-   <Link to="/create2">
-       <button type="submit"   className="btn btn-primary">Next</button>
-       </Link>
-        </Form>
-      </section>
+        <p>TODO: error</p>
 
       
 
