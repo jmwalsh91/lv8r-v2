237d93a3edd5bc9b76ee0efe96146583d76beed0
jmwalsh91 <jordanmwalsh@gmail.com>
2022-04-26 03:33:53 -0600

    handle form submit on page 2

--- app/utilities/pitchUtils.ts
+++ app/utilities/pitchUtils.ts
@@ -1,27 +1,45 @@
 import { isErrorResponse } from "@remix-run/react/data";
 import { dbClient } from "~/services/dbClient";
 
-export const createPitch = async ( form: FormData ) => {
-    let ownerId: FormDataEntryValue | null = await form.get("ownerId");
-    let pitchName: FormDataEntryValue | null = await form.get("productName")
-    
-    console.log("To love someone is to isolate him from the world, wipe out every trace of him, dispossess him of his shadow, drag him into a murderous future. It is to circle around the other like a dead star and absorb him into a black lightTo love someone is to isolate him from the world, wipe out every trace of him, dispossess him of his shadow, drag him into a murderous future. It is to circle around the other like a dead star and absorb him into a black light.To love someone is to isolate him from the world, wipe out every trace of him, dispossess him of his shadow, drag him into a murderous future. It is to circle around the other like a dead star and absorb him into a black light.To love someone is to isolate him from the world, wipe out every trace of him, dispossess him of his shadow, drag him into a murderous future. It is to circle around the other like a dead star and absorb him into a black light.To love someone is to isolate him from the world, wipe out every trace of him, dispossess him of his shadow, drag him into a murderous future. It is to circle around the other like a dead star and absorb him into a black light.")
+export const createPitch = async (form: FormData) => {
+  let ownerId: FormDataEntryValue | null = await form.get("ownerId");
+  let pitchName: FormDataEntryValue | null = await form.get("productName");
 
-    let { data: pitch, error } = await dbClient.from("Pitch").insert({
-        pitchName: pitchName
-    });
-    if (pitch) {
-        let updatedUser = await dbClient
-        .from("Users")
-        .update({ pitch: pitch[0].id })
-        .match({ owner: ownerId });
-        console.log(updatedUser)
-        return updatedUser
-    }
-    if (error) {
-      return console.log(error)
-    } 
+  console.log("page one");
 
-  
- 
-  };
+  let { data: pitch, error } = await dbClient.from("Pitch").insert({
+    pitchName: pitchName,
+  });
+  if (pitch) {
+    let updatedUser = await dbClient
+      .from("Users")
+      .update({ pitch: pitch[0].id })
+      .match({ owner: ownerId });
+    console.log(updatedUser);
+    return updatedUser;
+  }
+  if (error) {
+    return console.log(error);
+  }
+};
+
+export const insertPageTwo = async (form: FormData) => {
+  let ownerId: FormDataEntryValue | null = await form.get("ownerId");
+  let problemIntro: FormDataEntryValue | null = await form.get("problemIntro");
+  let problemInfo: FormDataEntryValue | null = await form.get("problemInfo");
+
+  console.log("page two");
+
+  let { data: pitch, error } = await dbClient
+    .from("Pitch")
+    .insert({
+      p2TextIntro: problemIntro,
+      p2TextInfo: problemInfo,
+    })
+    .match({ owner: ownerId });
+
+    console.log(pitch)
+
+    return pitch 
+
+};
