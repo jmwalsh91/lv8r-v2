dc7ac212b56b0cfb3d091a4e88137f37293a2582
jmwalsh91 <jordanmwalsh@gmail.com>
2022-04-26 23:48:59 -0600

    page3 loader and action

--- app/routes/dashboard/$user/createPitch/create3.tsx
+++ app/routes/dashboard/$user/createPitch/create3.tsx
@@ -1,26 +1,34 @@
 import { ActionFunction, LoaderFunction, redirect } from '@remix-run/node'
-import { Form } from '@remix-run/react';
+import { Form, useLoaderData } from '@remix-run/react';
 import React from 'react'
 import { supabaseStrategy } from '~/services/auth.server';
+import { getUsername } from '~/utilities/getUserInfo';
 import { insertPageThree, insertPageTwo } from '~/utilities/pitchUtils';
 
 type Props = {}
-export const loader: LoaderFunction = async ({request}) => {
+export const loader: LoaderFunction = async ({ request }) => {
+    console.log("loader");
+    let sessionData: any = await supabaseStrategy.checkSession(request);
+    if (sessionData !== undefined) {
+      console.log(sessionData);
+      return sessionData;
+    } else return redirect("/");
+  };
+  export const action: ActionFunction = async ({ request }) => {
+    let form: FormData = await request.clone().formData();
+    let id: FormDataEntryValue | null = form.get("ownerId");
+    let pitch = await insertPageThree(form);
+    let username = await getUsername(id);
+  
+    if (pitch) {
+      console.log(username);
+      return redirect(`dashboard/${username}/createPitch/create4`);
+    } else return Error("an error occured");
+  };
 
-const data = request.body
-    return data
-}
 
-export const action: ActionFunction = async ({request}) => {
-  const form = await request.formData()
-  let pitchResponse = await insertPageThree(form)
-  if (pitchResponse) {
-    return console.log(pitchResponse)
-  } else return Error("an error occurred")
-}
-
-
-function Create2({}: Props) {
+function Create3({}: Props) {
+    let user = useLoaderData();
   return (
     <div className="flex flex-col justify-items-center justify-center align-center">
       <div>
@@ -36,6 +44,7 @@ function Create2({}: Props) {
           What is the problem that your product solves?
         </p>
         <Form method="post">
+        <input type="hidden" value={user.user.id} name="ownerId"></input>
     <section className='flex flex-col justify-center items-center gap-4 mt-4'>
       <textarea className="textarea textarea-primary w-96" placeholder="solution intro" name="solutionIntro"></textarea>
       <textarea className="textarea textarea-primary w-96 h-40" placeholder="Elaborate on the solution!"></textarea>
@@ -49,4 +58,4 @@ function Create2({}: Props) {
   )
 }
 
-export default Create2
+export default Create3
